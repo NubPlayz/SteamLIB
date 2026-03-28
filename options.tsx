4082fc712300f9667f49040eaf488d1704497a28
NubPlayz <182065584+NubPlayz@users.noreply.github.com>
2026-03-28 19:45:24 +0530

    bare options

---
+++ options.tsx
@@ -0,0 +1,22 @@
+import "./options.css"
+
+function OptionsIndex() {
+  return (
+    <div className="options-container">
+      <div className="options-card">
+        <div className="header-small">Steamlib Preferences</div>
+        <h1 className="title-main">SteamLIB</h1>
+        <p className="last-updated"></p>
+
+        <section className="mirror-selection">
+          <h2>Current sources</h2>
+          <p className="description">
+           
+          </p>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default OptionsIndex
